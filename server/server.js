9030b724dc8423d37b1f2c377c002c8de5bbe923
jay6205 <dhananjaybalekar@gmail.com>
2026-01-14 13:12:23 +0530

    Fix Bug

--- server/server.js
+++ server/server.js
@@ -16,7 +16,14 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: (origin, callback) => {
+        const allowedOrigins = process.env.CLIENT_URL ? process.env.CLIENT_URL.split(',').map(origin => origin.trim()) : [];
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 
