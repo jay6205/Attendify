1f96e0c1c291e6b6f1444b1f5c1e7be137be6d6c
jay6205 <dhananjaybalekar@gmail.com>
2026-02-23 17:02:55 +0530

    Add Explicit Logout route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,12 @@
-
 import express from 'express';
-import { registerUser, loginUser, getMe, changePassword } from '../controllers/authController.js';
+import { registerUser, loginUser, getMe, changePassword, logoutUser } from '../controllers/authController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser); // Forces Student role
 router.post('/login', loginUser);
+router.post('/logout', logoutUser); // Handled by client-side token deletion
 router.put('/password', protect, changePassword);
 router.get('/me', protect, getMe);
 
